44abe304d28e8e1f5da60c8dfdb826d443fd9018
Al-E-00 <mazzellaalessio6@gmail.com>
2022-06-17 15:48:10 +0200

    generate mail done

---
+++ app.js
@@ -0,0 +1,21 @@
+new Vue ({
+    el:"#app",
+    data: {
+        mailList: [],
+        counterAjax: 0,
+    },
+    methods:{
+        generateMail(){
+            for (let i = 0; i < 10; i++){
+                this.mailList = [];
+                this.counterAjax ++;
+                axios
+                .get("https://flynn.boolean.careers/exercises/api/random/mail")
+                .then((axiosResp)=>{
+                    this.mailList.push(axiosResp.data.response);
+                })
+                console.log(this.counterAjax)
+            }
+        },
+    }
+})
